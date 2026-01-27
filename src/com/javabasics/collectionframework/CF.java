435ea49ab7f86f4b697c98a013a880584284a4f4
Vaibhav Gupta <vaibhavkrgupta369@gmail.com>
2026-01-27 22:40:44 +0530

    cc

--- src/com/javabasics/collectionframework/CF.java
+++ src/com/javabasics/collectionframework/CF.java
@@ -9,7 +9,7 @@ public class CF {
         //List used when allowing duplicate in order
         //Arraylist
 
-        List<Integer> arraylist= new ArrayList<>();
+        List<Integer> arraylist= new  ArrayList<>();
         arraylist.add(1);
         arraylist.add(2);
         arraylist.add(3);
